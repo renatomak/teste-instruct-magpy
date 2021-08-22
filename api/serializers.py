7cd2657ce8c7363c5593d00ce12963866a2e6e26
renatomak <renato.mark.silva@gmail.com>
2021-08-22 13:10:00 -0300

    implementado o metodo create

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
-
+import requests
 from .models import PackageRelease, Project
+
+
+def consult_package_pypi(name_pakage):
+    url = f'https://pypi.org/pypi/{name_pakage}/json'
+
+    res = requests.get(url=url)
+
+    if res.status_code >= 200 and res.status_code <= 299:
+        return {
+            "status": res.status_code,
+            "name": name_pakage,
+            "version": res.json()['info']['version']}
+    return {"status": res.status_code}
 
 
 class PackageSerializer(serializers.ModelSerializer):
@@ -17,9 +30,31 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     packages = PackageSerializer(many=True)
 
+    def generate_new_package(self, packages):
+        new_packages = []
+        for package in packages:
+            result = consult_package_pypi(package['name'])
+            if result['status'] != 200:
+                raise serializers.ValidationError({
+                    "error": "One or more packages doesn't exist"
+                })
+                return
+            if 'version' not in package:
+                package = {'name': package['name'],
+                           'version': result['version']}
+
+            new_packages.append(package)
+        return new_packages
+
     def create(self, validated_data):
-        # TODO
-        # - Processar os pacotes recebidos
-        # - Persistir informações no banco
-        packages = validated_data['packages']
+        new_packages = self.generate_new_package(validated_data["packages"])
+
+        validated_data["packages"] = new_packages
+        packages = validated_data["packages"]
+
+        project = Project.objects.create(name=validated_data['name'])
+        for package in packages:
+            PackageRelease.objects.create(
+                project=project, name=package['name'], version=package['version'])
+
         return Project(name=validated_data['name'])
