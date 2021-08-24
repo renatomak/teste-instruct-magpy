7cff9f582a0b5e3e69d45e2e041aa57ae0c0f6d6
renatomak <renato.mark.silva@gmail.com>
2021-08-23 23:11:21 -0300

    correçao da funçao de consulta a api pypi

--- api/serializers.py
+++ api/serializers.py
@@ -3,17 +3,27 @@ import requests
 from api.models import PackageRelease, Project
 
 
-def consult_package_pypi(name_pakage):
-    url = f'https://pypi.org/pypi/{name_pakage}/json'
+def consult_package_pypi(package):
+    name = package['name']
+
+    if "version" in package:
+        version = package['version']
+        url = f'https://pypi.org/pypi/{name}/{version}/json'
+    else:
+        url = f'https://pypi.org/pypi/{name}/json'
 
     res = requests.get(url=url)
+    if res.status_code != 200:
+        raise serializers.ValidationError({
+            "error": "One or more packages doesn't exist"
+        })
+    if "version" not in package:
+        version = res.json()['info']['version']
 
-    if res.status_code >= 200 and res.status_code <= 299:
-        return {
-            "status": res.status_code,
-            "name": name_pakage,
-            "version": res.json()['info']['version']}
-    return {"status": res.status_code}
+    return {
+        "status": res.status_code,
+        "name": name,
+        "version": version}
 
 
 class PackageSerializer(serializers.ModelSerializer):
@@ -33,16 +43,8 @@ class ProjectSerializer(serializers.ModelSerializer):
     def generate_new_package(self, packages):
         new_packages = []
         for package in packages:
-            result = consult_package_pypi(package['name'])
-            if result['status'] != 200:
-                raise serializers.ValidationError({
-                    "error": "One or more packages doesn't exist"
-                })
-            if 'version' not in package:
-                package = {'name': package['name'],
-                           'version': result['version']}
-
-            new_packages.append(package)
+            new_package = consult_package_pypi(package)
+            new_packages.append(new_package)
         return new_packages
 
     def create(self, validated_data):
