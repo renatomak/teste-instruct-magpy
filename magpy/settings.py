604cfc5cbaa4c2013f48c156ac47628f5b7b9a9d
renatomak <renato.mark.silva@gmail.com>
2021-08-24 15:00:15 -0300

    configuração inicial swagger ui

--- magpy/settings.py
+++ magpy/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- magpy/urls.py
+++ magpy/urls.py
@@ -17,13 +17,37 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework import routers
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from api import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Documentação API REST Magpy",
+        default_version='v1',
+        description="Documentação pública da API REST Magpy",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="renato.mark.silva@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 router = routers.SimpleRouter()
 router.register(r'projects', views.ProjectViewSet)
 router.register('', views.ProjectViewSet)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('', include(router.urls)),
